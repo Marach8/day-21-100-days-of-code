a461cfe7c8bbf4d944cafc05a28e48296c1de0f7
Marach8 <114676239+Marach8@users.noreply.github.com>
2023-07-20 08:35:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+print('Math Game!')
+print()
+a = int(input('Name your multiples: '))
+print()
+n = 0
+for i in range(1,11):
+  print()
+  b = i * a
+  c = int(input(f'{i} X {a} = '))
+  if c == b:
+    print('Awesome! you got it')
+    n += 1
+  else:
+    print(f'Nope! the correct answer is {b}')
+print()
+print(f'You scored {n}/10')
